8f00e6cba62b1d09b95a28a5aea8b9bc841f2caf
rayGreycloud <rayGreycloud@gmail.com>
2017-05-03 12:11:13 -0700

    Add first route definition

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -12,7 +13,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
 
