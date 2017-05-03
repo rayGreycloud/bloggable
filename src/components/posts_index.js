26f4e2979d1cf2cf88b1922026121c4c3e12b57d
rayGreycloud <rayGreycloud@gmail.com>
2017-05-03 12:03:53 -0700

    Add boilerplate PostsIndex component

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Posts Index</h3>  
+      </div>
+    );
+  }
+}
