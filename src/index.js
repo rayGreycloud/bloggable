1c69a46f4cda514b131620b29902b9c29bbcd4fe
rayGreycloud <rayGreycloud@gmail.com>
2017-05-03 09:46:11 -0700

    Add react-router-dom and test components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class TestComponentOne extends Component {
+  render() { return <div>This is the Test Component One.</div> }
+}
+
+class TestComponentTwo extends Component {
+  render() { return <div>This is the Test Component Two.</div> }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/test-component-one" component={TestComponentOne}/>
+        <Route path="/test-component-two" component={TestComponentTwo}/>
+      </div>
+    </BrowserRouter>
+{/*    <App /> */}
   </Provider>
   , document.querySelector('.container'));
