a8056ee8c3bf0f91ade1c4df12585efb07ec9a69
rayGreycloud <rayGreycloud@gmail.com>
2017-05-04 06:20:39 -0700

    Add render method for post list

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -7,13 +8,33 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">
+            {post.categories}
+          </span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    });
+  }
+
   render() {
     return (
       <div>
-        <h3>Posts Index</h3>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
