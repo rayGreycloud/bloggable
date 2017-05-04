8c3d7a79c763d869f40be2115e1f92ae227bdb9d
rayGreycloud <rayGreycloud@gmail.com>
2017-05-04 06:25:03 -0700

    Add boilerplate PostsNew component

---
+++ src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>New Post Form</div>
+    );
+  }
+}
+
+export default PostsNew;
