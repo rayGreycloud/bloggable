1a1cbcb429c9e6cbef2badef98a3075e580834e6
rayGreycloud <rayGreycloud@gmail.com>
2017-05-04 10:29:56 -0700

    Refactor to single renderField method for all form fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
       <div className="form-group">
-        <label>Title</label>
-        <input
-          className="form-control"
-          type="text"
-          {...field.input}
-        />
-      </div>
-    );
-  }
-
-  renderTagsField(field) {
-    return (
-      <div className="form-group">
-        <label>Categories</label>
+        <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
@@ -32,12 +19,14 @@ class PostsNew extends Component {
     return (
       <form>
         <Field
+          label="Title"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
         <Field
+          label="Tags"
           name="tags"
-          component={this.renderTagsField}
+          component={this.renderField}
         />
 
       </form>
