c6788936a6cf4980dbf29548e88a91250359db04
rayGreycloud <rayGreycloud@gmail.com>
2017-05-05 09:53:12 -0700

    Use ownProps in posts_show for component props

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchAPost(id);
+  }
+
   render() {
     return (
       <div>Posts List</div>
@@ -8,4 +15,8 @@ class PostsShow extends Component {
   };
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchAPost })(PostsShow);
