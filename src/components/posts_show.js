46abfafbed9c87c1f4bdd48a87b12945e2f608a7
rayGreycloud <rayGreycloud@gmail.com>
2017-05-05 08:42:10 -0700

    Scaffold posts_show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>Posts List</div>
+    );
+  };
+}
+
+export default PostsShow;
