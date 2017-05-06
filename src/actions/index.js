cc25c0b6b18d33f6ebef0dc90a04e4c195de5bac
rayGreycloud <rayGreycloud@gmail.com>
2017-05-06 06:26:44 -0700

    Add DELETE_POST action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,12 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_APOST = 'fetch_apost';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=BLOGGABLE1234';
-
-export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
-  return {
-    type: FETCH_POSTS,
-    payload: request
-  };
-}
 
 export function createPost(values, callback) {
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
@@ -26,11 +18,30 @@ export function createPost(values, callback) {
   };
 }
 
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
+
 export function fetchAPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
 
   return {
     type: FETCH_APOST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
