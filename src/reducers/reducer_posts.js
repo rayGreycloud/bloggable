615aec90402a951cfe447856591ad4f56b0813a0
rayGreycloud <rayGreycloud@gmail.com>
2017-05-06 06:32:21 -0700

    Add DELETE_POST reducer to remove deleted post from application state

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,17 +1,19 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_APOST } from '../actions';
+import { FETCH_POSTS, FETCH_APOST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    
+    case DELETE_POST:
+    // Return new state object that omits deleted post
+    return _.omit(state, action.payload);
+
     case FETCH_APOST:
-      // ES6 Refactor using key interpolation
       return { ...state, [action.payload.data.id]: action.payload.data };
-      // const post = action.payload.data;
-      // const newState =  { ...state  };
-      // newState[post.id] = post;
-      // return newState;
+
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+
     default:
       return state;
   }
